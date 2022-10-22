73d8e5c84b686e41ec91525b749b7894ef10c5a1
DanParSeg <44481578+DanParSeg@users.noreply.github.com>
2022-10-22 20:28:56 +0200

    desde móvil

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,9 +1,9 @@
 import time
 
 sudoku=[[3,4,1,0],
-        [0,2,0,0],
-        [0,0,2,0],
-        [0,1,4,3]]
+                [0,2,0,0],
+                [0,0,2,0],
+                [0,1,4,3]]
 
 def expand_sudoku(sudoku):
     """
@@ -49,10 +49,11 @@ def check_columns(sudoku_expanded):
             if (len(sudoku_expanded[j][i])>1): #if the value is not known
                 for k in range(len(known_column_values)):#remove known values
                     try:
-                        print("removing",known_column_values[k],"from",sudoku_expanded[j][i],"at",i,j)
-                        sudoku_expanded[i][j].remove(known_column_values[k])
-                        print("removed",known_column_values[k],"from",sudoku_expanded[j][i],"at",i,j)
+                        #print("removing",known_column_values[k],"from",sudoku_expanded[j][i],"at",i,j)
+                        sudoku_expanded[j][i].remove(known_column_values[k])
+                        #print("removed",known_column_values[k],"from",sudoku_expanded[j][i],"at",i,j)
                     except:
+                        #print(e)
                         pass
     return sudoku_expanded
 
@@ -61,19 +62,29 @@ def check_squares(sudoku_expanded):
     Checks the squares for possible values
     """
     #0,0 0,1 1,0 1,1   0,2 0,3 1,2 1,3   2,0 2,1 3,0 3,1   2,2 2,3 3,2 3,3
+    known_square_values=[]
+    for square in range(4):
+        known_square_values.append([])
+        for i in range(0,2):
+            for j in range(0,2):
+                #print(i+int(square%2)*2,j+int(square/2)*2)
+                i2=i+int(square%2)*2
+                j2=j+int(square/2)*2
+                if(len(sudoku_expanded[i2][j2])==1):
+                    known_square_values[square].append(sudoku_expanded[i2][j2][0])
+                         
+    #print(known_square_values)       
     for square in range(4):
         for i in range(0,2):
             for j in range(0,2):
-                #print(i+int(k%2)*2,j+int(k/2)*2)
+                #print(i+int(square%2)*2,j+int(square/2)*2)
                 i2=i+int(square%2)*2
                 j2=j+int(square/2)*2
-                known_square_values=[]
-                if(len(sudoku_expanded[i2][j2])==1):
-                    known_square_values.append(sudoku_expanded[i2][j2][0])
                 if(len(sudoku_expanded[i2][j2])>1):
-                    for k in range(len(known_square_values)):#remove known values
+                    for k in range(len(known_square_values[square])):#remove known values
                         try:
-                            sudoku_expanded[i][j].remove(known_square_values[k])
+                            #print("removing",known_square_values[square][k],"from",sudoku_expanded[j][i],"at",i,j)
+                            sudoku_expanded[i2][j2].remove(known_square_values[square][k])
                         except:
                             pass
     return sudoku_expanded
@@ -89,8 +100,9 @@ def check_sudoku(sudoku_expanded):
     print("column")
     sudoku_expanded=check_columns(sudoku_expanded)
     print_sudoku(sudoku_expanded)
-    #print("column",sudoku_expanded)
-    #sudoku_expanded=check_squares(sudoku_expanded)
+    print("squares")
+    sudoku_expanded=check_squares(sudoku_expanded)
+    print_sudoku(sudoku_expanded)
     return sudoku_expanded
 
 def is_solved(sudoku_expanded):
@@ -122,7 +134,7 @@ def solve_sudoku(sudoku):
     Solves the sudoku
     """
     sudoku_expanded=expand_sudoku(sudoku)
-    print(sudoku_expanded)
+    #print_sudoku(sudoku)
     while not is_solved(sudoku_expanded):
         sudoku_expanded=check_sudoku(sudoku_expanded)
         time.sleep(1)
@@ -131,4 +143,4 @@ def solve_sudoku(sudoku):
 sudoku_expanded=solve_sudoku(sudoku)
 
 
-#check_squares(expand_sudoku(sudoku))
+#check_sudoku(expand_sudoku(sudoku))
